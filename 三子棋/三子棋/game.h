691ca25b33de97a5cfc78c249a5d2a004368420f
JoyceYIDA <1923589855@qq.com>
2018-11-18 20:26:15 +0800

    有些问题 还需要改进

---
+++ 三子棋/三子棋/game.h
@@ -0,0 +1,14 @@
+
+#ifndef _GAME_H__
+#define _GAME_H__
+#define ROW 3
+#define COL 3
+void initboard(char board[ROW][COL]);
+void displayboard(char board[ROW][COL]);
+void menu();
+void test();
+void playermove(char board[ROW][COL]);
+void computermove(char board[ROW][COL]);
+char win(char board[ROW][COL]);
+int full(char board[ROW][COL]);
+#endif
---
+++ 三子棋/三子棋/test.c
@@ -0,0 +1,68 @@
+#define _GRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"game.h"
+#include<time.h>
+#include<Windows.h>
+void menu()
+{
+	printf("*************************\n");
+	printf("******1.play 0.exit******\n");
+	printf("*************************\n");
+}
+void game()
+{
+	char ret = 0;
+	char board[ROW][COL] = { 0 };
+	initboard(board,ROW,COL);
+	displayboard(board, ROW, COL);
+	do
+	{
+		printf("玩家走\n");
+		playermove(board, ROW, COL);
+		displayboard(board, ROW, COL);
+		printf("电脑走\n");
+		computermove(board, ROW, COL);
+		displayboard(board, ROW, COL);
+		break;
+		ret = win(board[ROW][COL]);
+	}
+		while (ret == ' ');
+		if (ret == 'o')
+			printf("恭喜你，你赢啦！\n");
+		else if (ret == 'x')
+			printf("很遗憾，电脑赢了\n");
+		else if (ret == 'q')
+			printf("平局啦！\n");
+			displayboard(board, ROW, COL);
+}//判断输赢之后打印棋盘
+
+void test()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择");
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:printf("选择错误,请重新选择\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	system("pause");
+	return 0;
+}
